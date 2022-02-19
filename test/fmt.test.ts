ef7bcc91027d66d11a0232c9b69d1ec0c579428a
CJ <cj@specialblend.dev>
2022-02-19 14:17:19 -0600

    chore: test fmt.ts

---
+++ test/fmt.test.ts
@@ -0,0 +1,40 @@
+import { fmtl, Labels } from "../src";
+
+describe("fmtl", () => {
+  type FmtLabelsTest = [string, { input: Record<any, any>; output: Labels }];
+  const fmtl_tests: FmtLabelsTest[] = [
+    [
+      "already label",
+      {
+        input: { foo: "bar" },
+        output: { foo: "bar" },
+      },
+    ],
+    [
+      "nested record",
+      {
+        input: { foo: "bar", baz: { faz: "foo" } },
+        output: { foo: "bar" },
+      },
+    ],
+    [
+      "shallow record with empty",
+      {
+        input: { foo: "bar", baz: "" },
+        output: { foo: "bar" },
+      },
+    ],
+    [
+      "nested record with empty",
+      {
+        input: { foo: "bar", baz: { faz: "foo" }, bar: "" },
+        output: { foo: "bar" },
+      },
+    ],
+  ];
+  describe("it formats object into shallow labels", () => {
+    test.each(fmtl_tests)("%s", (_, test_case) => {
+      expect(fmtl(test_case.input)).toEqual(test_case.output);
+    });
+  });
+});
