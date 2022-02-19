e67bcce98003d069420e1619d484746f1d5019df
CJ <cj@specialblend.dev>
2022-02-19 15:12:04 -0600

    fix: clone tagged obj

--- src/tag.ts
+++ src/tag.ts
@@ -7,10 +7,10 @@ export function Tagged<T, S extends symbol, V>(
   value: V,
   x: T
 ): Tagged<T, S, V> {
-  return Object.assign(x, { [tag]: value });
+  return Object.assign({}, x, { [tag]: value });
 }
 
-export function istagged<T, S extends symbol, V>(
+export function isTagged<T, S extends symbol, V>(
   tag: S,
   x: Tagged<T, S, V> | T
 ): x is Tagged<T, S, V> {
